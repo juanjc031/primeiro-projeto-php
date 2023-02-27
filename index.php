55c437554965430fb2cca00a2728240c9be60524
Juan Costa <104514317+juanjc031@users.noreply.github.com>
2023-02-27 16:46:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "thiago bosta" ?> </h1>
+    <h2><?php echo 2*2 ?></h2>
+    <h2><?php echo 2+8 ?></h2>
+    <h2><?php echo 7-2 ?></h2>
+    <h2><?php echo 6/2 ?></h2>
+
+
+</body>
+</html>
